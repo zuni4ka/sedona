cb1872f7168876d584bb3831ad0c23ecfbe6d883
Maria Plieshka <zuni4ka@gmail.com>
2019-01-16 23:58:41 +0100

    Сделана анимация формы поиска

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
     var searchFormBody = document.querySelector('.search-form-body');
 
     var onSearchButtonClick = function (){
-      searchFormBody.classList.toggle('visually-hidden');
+      searchFormBody.classList.toggle('search-body-hidden');
     };
     document.querySelector('.search-form-button').addEventListener('click', onSearchButtonClick);
     
